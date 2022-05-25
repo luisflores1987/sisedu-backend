209c04c1c7c2c61ca3ac49948b17c570568e6487
luisflores1987 <luis_flores_1987@hotmail.com>
2022-05-25 13:22:17 -0500

    init sisedu v3

---
+++ src/main/java/com/apiservi/siseduBackend/SiseduBackendApplication.java
@@ -0,0 +1,13 @@
+package com.apiservi.siseduBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiseduBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiseduBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apiservi/siseduBackend/SiseduBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apiservi.siseduBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiseduBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
